910369113df9a1beb15a2ad0a5686e62ae1154f4
Andrii Chornyi <andchorniy@gmail.com>
2021-02-21 00:53:57 +0100

    update

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
-const daysRef = document.querySelector('span[data-value="days"]');
-const hoursRef = document.querySelector('span[data-value="hours"]');
-const minsRef = document.querySelector('span[data-value="mins"]');
-const secsRef = document.querySelector('span[data-value="secs"]');
+
 
 class CountdownTimer { 
     constructor({ selector, targetDate }) { 
         this.selector = selector
         this.targetDate = targetDate
+        createMarup(this.selector) 
         setInterval(() => { 
-        
+        const daysRef = document.querySelector(`#${this.selector} span[data-value="days"]`);
+        const hoursRef = document.querySelector(`#${this.selector} span[data-value="hours"]`);
+        const minsRef = document.querySelector(`#${this.selector} span[data-value="mins"]`);
+        const secsRef = document.querySelector(`#${this.selector} span[data-value="secs"]`);
+         
         const delta = this.targetDate - Date.now() 
 
         daysRef.textContent = Math.floor(delta / (1000 * 60 * 60 * 24));
@@ -20,7 +22,40 @@ class CountdownTimer {
 
     }
 }
+function createMarup(selector) { 
+    document.body.insertAdjacentHTML('beforeend', `<div class="timer" id="${selector}">
+    <div class="field">
+        <span class="value" data-value="days">11</span>
+        <span class="label">Days</span>
+    </div>
+
+    <div class="field">
+        <span class="value" data-value="hours">11</span>
+        <span class="label">Hours</span>
+    </div>
+
+    <div class="field">
+        <span class="value" data-value="mins">11</span>
+        <span class="label">Minutes</span>
+    </div>
+
+    <div class="field">
+        <span class="value" data-value="secs">11</span>
+        <span class="label">Seconds</span>
+    </div>
+</div>`)
+}
 new CountdownTimer({
-  selector: '#timer-1',
-  targetDate: new Date('Jul 17, 2021'),
+  selector: 'timer-1',
+  targetDate: new Date('Sep 26, 2021'),
+});
+
+new CountdownTimer({
+  selector: 'timer-2',
+  targetDate: new Date('Oct 1, 2021'),
+});
+
+new CountdownTimer({
+  selector: 'timer-3',
+  targetDate: new Date('May 21, 2021'),
 });
